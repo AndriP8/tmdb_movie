d93d5dc7443c3396449a8b5272c4048c5fdbc201
Andri Purnomo <56879512+AndriP8@users.noreply.github.com>
2021-11-13 21:00:00 +0700

    add services api call

---
+++ src/services/index.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const callAPI = async ({ url, method, data }) => {
+  const response = await axios({ url, method, data }).catch(
+    (err) => err.response
+  );
+  const res = {
+    error: false,
+    message: response.data.message,
+    data: response.data,
+  };
+  return res;
+};
+
+export default callAPI;
