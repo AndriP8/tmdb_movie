ef92fbe32657f515cd2a7ad055edfe93f78a634f
Andri Purnomo <56879512+AndriP8@users.noreply.github.com>
2021-11-15 22:09:14 +0700

    update minnor Banner HomePage

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -24,21 +24,29 @@ function MainBanner() {
   return (
     <section className="h-[60vh] md:h-[90vh] relative">
       <div className="absolute z-[-1] inset-0 h-full w-full">
-        <img
-          src={`${API_IMG}/${movie.backdrop_path}`}
-          className="h-full w-full object-fill"
-          alt="banner"
-        />
+        {!movie?.backdrop_path ? (
+          <img
+            src={`${API_IMG}/${movie?.poster_path}`}
+            className="h-full w-full object-fill"
+            alt="banner"
+          />
+        ) : (
+          <img
+            src={`${API_IMG}/${movie?.backdrop_path}`}
+            className="h-full w-full object-fill"
+            alt="banner"
+          />
+        )}
       </div>
       <div className="px-16 w-full absolute top-5 sm:top-36 bottom-5">
         <div className="text-white font-roboto">
-          <h2 className="text-4xl md:text-5xl font-bold">{movie.title}</h2>
+          <h2 className="text-4xl md:text-5xl font-bold">{movie?.title}</h2>
           <p className="text-lg sm:text-2xl font md:pr-40 my-6">
-            {movie.overview}
+            {movie?.overview}
           </p>
           <div className="flex items-center">
             <StarIcon className="h-7 w-7 mr-2" />
-            <span className="text-xl font-bold">{movie.vote_average}</span>
+            <span className="text-xl font-bold">{movie?.vote_average}</span>
           </div>
         </div>
       </div>
