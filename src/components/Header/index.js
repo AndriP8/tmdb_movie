bbf0486b5cc1798cb42a70390058019f672a30b3
Andri Purnomo <56879512+AndriP8@users.noreply.github.com>
2021-11-21 20:13:13 +0700

    refactor navbar to clean code

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,34 +1,14 @@
-import { useState, useCallback } from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { SearchIcon } from "@heroicons/react/outline";
-import { getSearchMovie } from "../../services/movie";
-import { useDispatch } from "react-redux";
-import { searchMovie } from "../../redux/actionTypes/search";
-
 function Navbar() {
-  const dispatch = useDispatch();
   const router = useHistory();
 
   const [searchValue, setSearchValue] = useState("");
-  const [searchQuery, setSearchQuery] = useState("");
-
-  const getSearch = useCallback(
-    async (value) => {
-      const response = await getSearchMovie(value);
-      dispatch(searchMovie(response.data.results));
-    },
-    [dispatch]
-  );
-
-  const onSearchChange = (e) => {
-    setSearchValue(e.target.value);
-    setSearchQuery(e.target.value);
-  };
 
   const onSearch = (e) => {
     e.preventDefault();
-    getSearch(searchValue);
-    router.push(`/search?q=${searchQuery}`);
+    router.push(`/search?q=${searchValue}`);
   };
 
   return (
@@ -46,7 +26,7 @@ function Navbar() {
               className="bg-mainTransparent rounded-lg py-2 pr-0 sm:pr-12 pl-4 focus:outline-none focus:ring-2 focus:ring-blue-800 w-full sm:inline-block"
               placeholder="Search Movie"
               value={searchValue}
-              onChange={(e) => onSearchChange(e)}
+              onChange={(e) => setSearchValue(e.target.value)}
             />
             <SearchIcon className="w-6 h-6 absolute -top-3 right-2 hidden md:inline-block sm:top-2 sm:right-2" />
           </form>
